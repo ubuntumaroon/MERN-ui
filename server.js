58029fa7cb70061e1e35bbed9c565b92ea1c9486
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-05-22 10:34:09 -0400

    add api proxy

--- server.js
+++ server.js
@@ -3,16 +3,25 @@
  */
 require('dotenv').config();
 const express = require('express');
+const {createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(express.static('public'));
 
-// send env to front end
+// setup proxy to api
+const apiProxyTarget = process.env.API_PROXY_TARGET;
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
+if (apiProxyTarget) {
+  app.use(UI_API_ENDPOINT, createProxyMiddleware({ target: apiProxyTarget }));
+}
+
+// send API env to front end
 const env = { UI_API_ENDPOINT };
 app.get('/env.js', function(req, res) {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
+
+
 
 const port = process.env.UI_SERVER_PORT || 8000;
 app.listen(port, function () {
