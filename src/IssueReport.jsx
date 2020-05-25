0427df5beb507f0f4941cba36347ad083a07db07
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-05-25 17:20:47 -0400

    add hash route

---
+++ src/IssueReport.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function IssueReport() {
+  return (
+    <div>
+      <h2>View for Issue detail report</h2>
+    </div>
+  );
+}
--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,15 @@ import 'regenerator-runtime';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
-import IssueList from './IssueList.jsx';
+import Page from './Page.jsx';
 
-const element = <IssueList />;
+const element = (
+  <Router>
+    <Page />
+  </Router>
+);
 ReactDOM.render(element, document.getElementById('contents'));
 
 /** HMR */
