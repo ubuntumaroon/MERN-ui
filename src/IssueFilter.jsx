d7409a36fe097e1aca87ec40a27540dd101f08bd
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-05-27 21:39:37 -0400

    add filter status

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -2,13 +2,35 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 class IssueFilter extends React.Component {
-  constructor() {
+  constructor({ location: { search } }) {
     super();
+    const params = new URLSearchParams(search);
+    this.state = {
+      status: params.get('status') || '',
+      changed: false,
+    };
+
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+    this.showOriginalFilter = this.showOriginalFilter.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+
+    if (prevSearch !== search) {
+      this.showOriginalFilter();
+    }
   }
 
   onChangeStatus(e) {
     const status = e.target.value;
+    this.setState({ status, changed: true });
+  }
+
+  applyFilter() {
+    const { status } = this.state;
     const { history } = this.props;
     history.push({
       pathname: '/issues',
@@ -16,18 +38,32 @@ class IssueFilter extends React.Component {
     });
   }
 
+  showOriginalFilter() {
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
+    this.setState({
+      status: params.get('status') || '',
+      changed: false,
+    });
+  }
+
   render() {
+    const { status, changed } = this.state;
     return (
       <div>
         Status:
         {' '}
-        <select onChange={this.onChangeStatus}>
+        <select value={status} onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
           <option value="Fixed">Fixed</option>
           <option value="Closed">Closed</option>
         </select>
+        {' '}
+        <button type="button" onClick={this.applyFilter}>Apply</button>
+        {' '}
+        <button type="button" onClick={this.showOriginalFilter} disabled={!changed}>Reset</button>
       </div>
     );
   }
