d4a71a567ec8c8d22db714ddf26ee9fb4677bc2b
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-06-01 15:47:00 -0400

    add navbar

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import URLSearchParams from '@ungap/url-search-params';
 import { Route } from 'react-router-dom';
-import { Badge as Label } from 'react-bootstrap';
 
 import IssueFilter from './IssueFilter.jsx';
 import IssueTable from './IssueTable.jsx';
@@ -123,7 +122,6 @@ export default class IssueList extends React.Component {
     const { match } = this.props;
     return (
       <>
-        <h1><Label variant="primary">Issue Tracker</Label></h1>
         <IssueFilter />
         <hr />
         <IssueTable issues={issues} closeIssue={this.closeIssue} deleteIssue={this.deleteIssue} />
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {
+  Navbar, Nav, NavDropdown, Tooltip, OverlayTrigger,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaPlus } from 'react-icons/fa';
 
 import Contents from './Contents.jsx';
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink exact to="/">Home</NavLink>
-      {' | '}
-      <NavLink to="/issues">Issue list</NavLink>
-      {' | '}
-      <NavLink to="/report">Report</NavLink>
-    </nav>
+    <>
+      <Navbar expand="md" bg="light" variant="light">
+        <Navbar.Brand>Issue Tracker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer exact to="/">
+              <Nav.Link>
+                Home
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/issues">
+              <Nav.Link>Issue List</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/report">
+              <Nav.Link>Report</Nav.Link>
+            </LinkContainer>
+          </Nav>
+
+          <Nav>
+            <Nav.Link>
+              <OverlayTrigger placement="left" delayShow={1000} overlay={<Tooltip id="create-issue">Create Issue</Tooltip>}>
+                <FaPlus />
+              </OverlayTrigger>
+            </Nav.Link>
+            <NavDropdown title="More" id="user-dropdown">
+              <NavDropdown.Item>About</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <small>
+      <p className="text-center">
+        Book project of &quot;MERN&quot; book.
+      </p>
+    </small>
   );
 }
 
@@ -20,6 +59,7 @@ export default function Page() {
     <div>
       <NavBar />
       <Contents />
+      <Footer />
     </div>
   );
 }
