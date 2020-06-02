2d9d5d422dccb4f811eca5255dd6e905f1da449a
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-06-02 10:55:51 -0400

    add form style

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import {
-  Button, ButtonToolbar, Form, InputGroup,
+  Button, ButtonToolbar, Form, InputGroup, Col, ButtonGroup,
 } from 'react-bootstrap';
 
 class IssueFilter extends React.Component {
@@ -84,30 +84,39 @@ class IssueFilter extends React.Component {
     const { status, changed } = this.state;
     const { effortMin, effortMax } = this.state;
     return (
-      <div>
-        <Form.Group>
-          <Form.Label>Status:</Form.Label>
-          <Form.Control as="select" value={status} onChange={this.onChangeStatus}>
-            <option value="">(All)</option>
-            <option value="New">New</option>
-            <option value="Assigned">Assigned</option>
-            <option value="Fixed">Fixed</option>
-            <option value="Closed">Closed</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Effect Between:</Form.Label>
-          <InputGroup>
-            <Form.Control value={effortMin} onChange={this.onChangeEffortMin} />
-            <InputGroup.Text>-</InputGroup.Text>
-            <Form.Control value={effortMax} onChange={this.onChangeEffortMax} />
-          </InputGroup>
-        </Form.Group>
-        <ButtonToolbar>
-          <Button variant="primary" onClick={this.applyFilter}>Apply</Button>
-          <Button variant="secondary" onClick={this.showOriginalFilter} disabled={!changed}>Reset</Button>
-        </ButtonToolbar>
-      </div>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} xs={6} sm={4} md={3} lg={2}>
+            <Form.Label>Status:</Form.Label>
+            <Form.Control as="select" value={status} onChange={this.onChangeStatus}>
+              <option value="">(All)</option>
+              <option value="New">New</option>
+              <option value="Assigned">Assigned</option>
+              <option value="Fixed">Fixed</option>
+              <option value="Closed">Closed</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Col} xs={6} sm={4} md={3} lg={2}>
+            <Form.Label>Effect Between:</Form.Label>
+            <InputGroup>
+              <Form.Control value={effortMin} onChange={this.onChangeEffortMin} />
+              <InputGroup.Text>-</InputGroup.Text>
+              <Form.Control value={effortMax} onChange={this.onChangeEffortMax} />
+            </InputGroup>
+          </Form.Group>
+          <Col xs={6} sm={4} md={3} lg={2}>
+            <Form.Label>&nbsp;</Form.Label>
+            <ButtonToolbar>
+              <ButtonGroup className="mr-2">
+                <Button variant="primary" onClick={this.applyFilter} size="sm">Apply</Button>
+              </ButtonGroup>
+              <ButtonGroup>
+                <Button variant="secondary" onClick={this.showOriginalFilter} disabled={!changed}>Reset</Button>
+              </ButtonGroup>
+            </ButtonToolbar>
+          </Col>
+        </Form.Row>
+      </Form>
     );
   }
 }
