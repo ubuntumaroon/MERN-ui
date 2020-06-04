e38f215f77350df608c66c6c5665b055a72a0736
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-06-04 13:07:48 -0400

    remove unecessary import

--- src/IssueAdd.jsx
+++ src/IssueAdd.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button, ButtonGroup } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 export default class IssueAdd extends React.Component {
   constructor() {
