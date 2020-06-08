198c176c64cfbcab1687eab68fe81a78b62a9d71
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-06-07 21:42:29 -0400

    finish server about render

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import store from './store.js';
 
-export default function About() {
-  return (
-    <div className="text-center">
-      <h3>Issue tracker App</h3>
-      <h4>{store.initialData ? store.initialData.about : 'Unknown'}</h4>
-    </div>
-  );
+import store from './store.js';
+import graphQLFetch from './graphQLFetch.js';
+
+export default class About extends React.Component {
+  static async fetchData() {
+    const data = await graphQLFetch('query {about}');
+    return data;
+  }
+
+  constructor(props) {
+    super(props);
+    const apiAbout = store.initialData ? store.initialData.about : null;
+    this.state = { apiAbout };
+  }
+
+  async componentDidMount() {
+    const { apiAbout } = this.state;
+    if (apiAbout === null) {
+      const data = await About.fetchData();
+      this.setState({ apiAbout: data.about });
+    }
+  }
+
+  render() {
+    const { apiAbout } = this.state;
+    return (
+      <div className="text-center">
+        <h3>Issue tracker App</h3>
+        <h4>{ apiAbout }</h4>
+      </div>
+    );
+  }
 }
