e496df5f74867a649135fd4257f638258ea5bf14
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-06-08 14:00:53 -0400

    fix setState warning

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -8,12 +8,43 @@ import IssueTable from './IssueTable.jsx';
 import IssueDetail from './IssueDetails.jsx';
 import graphQLFetch from './graphQLFetch.js';
 import Toast from './Toast.jsx';
+import store from './store.js';
 
 export default class IssueList extends React.Component {
+  static async fetchData(match, search, showError) {
+    const params = new URLSearchParams(search);
+    const vars = {};
+    if (params.get('status')) vars.status = params.get('status');
+    const effortMin = parseInt(params.get('effortMin'), 10);
+    if (!Number.isNaN(effortMin)) vars.effortMin = effortMin;
+    const effortMax = parseInt(params.get('effortMax'), 10);
+    if (!Number.isNaN(effortMax)) vars.effortMax = effortMax;
+
+    const query = `query issuelist(
+      $status: StatusType
+      $effortMin: Int
+      $effortMax: Int
+    ) {
+      issueList(
+        status: $status
+        effortMin: $effortMin
+        effortMax: $effortMax
+      ) {
+        _id id title status owner
+        created effort due
+      }
+    }`;
+
+    const data = await graphQLFetch(query, vars, showError);
+    return data;
+  }
+
   constructor() {
     super();
+    const issues = store.initialData ? store.initialData.issueList : null;
+    delete store.initialData;
     this.state = {
-      issues: [],
+      issues,
       toastVisible: false,
       toastMessage: '',
       toastType: 'success',
@@ -28,7 +59,8 @@ export default class IssueList extends React.Component {
   }
 
   componentDidMount() {
-    this.loadData();
+    const { issues } = this.state;
+    if (issues == null) this.loadData();
   }
 
   componentDidUpdate(prevProps) {
@@ -77,9 +109,9 @@ export default class IssueList extends React.Component {
       this.setState((prevState) => {
         const newList = [...prevState.issues];
         newList[index] = data.issueUpdate;
-        this.showSuccess(`Issue ${issues[index].id} was closed successfully`);
         return { issues: newList };
       });
+      this.showSuccess(`Issue ${issues[index].id} was closed successfully`);
     } else {
       this.loadData();
     }
@@ -99,30 +131,8 @@ export default class IssueList extends React.Component {
 
   async loadData() {
     const { location: { search } } = this.props;
-    const params = new URLSearchParams(search);
-    const vars = {};
-    if (params.get('status')) vars.status = params.get('status');
-    const effortMin = parseInt(params.get('effortMin'), 10);
-    if (!Number.isNaN(effortMin)) vars.effortMin = effortMin;
-    const effortMax = parseInt(params.get('effortMax'), 10);
-    if (!Number.isNaN(effortMax)) vars.effortMax = effortMax;
 
-    const query = `query issuelist(
-      $status: StatusType
-      $effortMin: Int
-      $effortMax: Int
-    ) {
-      issueList(
-        status: $status
-        effortMin: $effortMin
-        effortMax: $effortMax
-      ) {
-        _id id title status owner
-        created effort due
-      }
-    }`;
-
-    const data = await graphQLFetch(query, vars, this.showError);
+    const data = await IssueList.fetchData(null, search, this.showError);
     if (data) {
       this.setState({ issues: data.issueList });
     }
@@ -131,6 +141,8 @@ export default class IssueList extends React.Component {
   render() {
     const { issues } = this.state;
     const { match } = this.props;
+
+    if (issues == null) return null;
 
     const { toastMessage, toastType, toastVisible } = this.state;
     return (
